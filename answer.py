48e70b66e23add7daf5776fecc2da05fbe8a1616
chopeen <chopeen@gmail.com>
2020-01-08 10:41:33 +0100

    defined a set of test questions for the Bevacizumab article;

--- answer.py
+++ answer.py
@@ -4,18 +4,31 @@ from transformers import pipeline
 
 
 def main():
-    url = r'https://en.m.wikipedia.org/wiki/Bevacizumab'
-    text = get_text(url)
-    print(text)
-
-    exit(1)
+    url = r'https://en.m.wikipedia.org/wiki/Bevacizumab?action=render'
 
     nlp = pipeline('question-answering')
+    text = get_text(url)
 
-    text = 'He lived in London and his name was Jack.'
+    questions = [
+        'When was Bevacizumab approved in the United States?',
+        'What is the trade name of Bevacizumab?',
+        'What trade name is Bevacizumab sold under?',
+        'How much does a dose cost?',
+        'How does Bevacizumab work?',
+        'What are the common side effects of Bevacizumab?',
+        'How is Bevacizumab given?',
+        'How was Bevacizumab derived?',
+        'Is Bevacizumab an antibody?',
+        'What is a specialty drug?'
+    ]
 
-    answer = nlp(context=text, question='What was his name?')
-    print(answer)
+    for question in questions:
+        answer = get_answer(nlp, text, question)
+        print(f'{question} - {answer}')
+
+
+def get_answer(nlp, text, question):
+    return nlp(context=text, question=question)
 
 
 def get_text(url):
