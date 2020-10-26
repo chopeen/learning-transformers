69f91197402aba6b65cfb0cebe3a854ddeea8b85
chopeen <chopeen@gmail.com>
2020-10-26 16:47:55 +0100

    Compare a single base sentence

--- sentence_vectors.py
+++ sentence_vectors.py
@@ -5,28 +5,27 @@ Source: https://www.sbert.net/docs/usage/semantic_textual_similarity.html
 '''
 
 MODEL_NAME = 'distilbert-base-nli-stsb-mean-tokens'
-SENTENCES1 = [
-    'The cat sits outside',
-    'A man is playing guitar',
-    'The new movie is awesome'
+SENTENCES = [
+    'valve leaking',
+    'valves leaking',
+    'valve leakage',
+    'water dripping',
+    'reagent seeping from a broken valve',
+    'worn rubber gasket'
 ]
-SENTENCES2 = [
-    'The dog plays in the garden',
-    'A woman watches TV',
-    'The new movie is so great'
-]
+BASE_SENTENCE = ['valve leaking'] * len(SENTENCES)
 
 
 def main():
     model = SentenceTransformer(MODEL_NAME)
 
-    embeddings1 = model.encode(SENTENCES1, convert_to_tensor=True)
-    embeddings2 = model.encode(SENTENCES2, convert_to_tensor=True)
+    embeddings1 = model.encode(BASE_SENTENCE, convert_to_tensor=True)
+    embeddings2 = model.encode(SENTENCES, convert_to_tensor=True)
 
     cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
 
-    for i in range(len(SENTENCES1)):
-        print(f'{SENTENCES1[i]} \t {SENTENCES2[i]} \t Score: {cosine_scores[i][i]:.4f}')
+    for i in range(len(BASE_SENTENCE)):
+        print(f'Score: {cosine_scores[i][i]:.4f} \t {BASE_SENTENCE[i]} \t {SENTENCES[i]}')
 
 
 if __name__ == '__main__':
